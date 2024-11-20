4322c7ad8235696954296abf17188c16f268cd95
dim4ss <dimasghifaria@gmail.com>
2024-11-20 11:42:36 +0700

    menambah dua file

---
+++ latihan/looping/latihan4.py
@@ -0,0 +1,8 @@
+#program untuk menampilkan data angka sebanyak 10 kali menggunakan for 
+
+#for asep in range(10):
+ #   print("asep")
+
+#menampilkan angka genap 1 samapi 100 
+for ujang in range(2,101,2):
+    print(ujang)
